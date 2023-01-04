864e732d42afc36347096d094371f54f6e3ce96c
malkaviano <rafael.kss@gmail.com>
2023-01-04 20:47:08 -0300

    change: remove event store

--- src/infra/infra.module.ts
+++ src/infra/infra.module.ts
@@ -9,7 +9,6 @@ import {
 import { ItemCatalogStore } from '@infra/stores/catalogs/item-catalog.store';
 import { InventoryStore } from '@root/infra/stores/inventory.store';
 import { AmqpClient } from '@infra/clients/amqp.client';
-import { EventStore } from '@infra/stores/event.store';
 
 @Global()
 @Module({
@@ -18,7 +17,6 @@ import { EventStore } from '@infra/stores/event.store';
     { provide: ITEM_CATALOG_STORE_TOKEN, useClass: ItemCatalogStore },
     { provide: INVENTORY_STORE_TOKEN, useClass: InventoryStore },
     AmqpClient,
-    { provide: EVENT_STORE_TOKEN, useClass: EventStore },
   ],
   exports: [ITEM_CATALOG_STORE_TOKEN, INVENTORY_STORE_TOKEN, EVENT_STORE_TOKEN],
 })
